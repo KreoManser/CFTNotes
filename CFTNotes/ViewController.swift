9347b80fca6648ec98a355ab423c975c2f1fd540
KreoManser <sherba.ru@list.ru>
2023-02-03 13:57:17 +0300

    init app project CFTNotes

---
+++ CFTNotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CFTNotes
+//
+//  Created by Сергей Бабич on 03.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
